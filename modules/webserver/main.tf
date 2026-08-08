08706a8b175c5d9737d5a20ee10fe6e2f3b38277
solomon <ohiozeberyl@gmail.com>
2026-08-08 02:11:19 +0100

    mod1

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "ec2-sg" {
    egress {
     from_port   = 443
     to_port     = 443
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
