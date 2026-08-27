f5144419941d867af838cbb11521b31859dd9022
solomon <ohiozeberyl@gmail.com>
2026-08-27 18:25:57 +0100

    amended provider

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-terraform {
-  #required_version = "~> 1.16.0"
+# terraform {
+#   required_version = "~> 1.16.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.62"
-    }
-  }
-}
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.62"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.aws_region
